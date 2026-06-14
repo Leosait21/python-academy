3fbba9f48b6bff5bcb7db06e9fb7e5e57db2ff74
Leosait21 <mehrobsuhrob997@gmail.com>
2026-06-15 00:13:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ HTML = """
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<meta name="google-site-verification" content="v1PzejX5ey60Z5Y_8JEWPIzwRmTXIfcLQPmj0MLYKgs" />
+
 <title>Курсы программирования Python с нуля</title>
 
 <style>
